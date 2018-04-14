8d7d6db479fe5b230da5458d24a5c98848d2ac1b
pjgaudr <gaudp00@yahoo.com>
2018-04-14 09:14:21 -0400

    Added validation of server public key

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -2,10 +2,33 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
 import { Http, Headers, Response } from '@angular/http';
+declare var KJUR:any;
 
 @Injectable()
 export class AuthenticationService {
     constructor(private http: Http) { }
+
+    validatePublicKey() {
+      var publicKeyUrl = "http://localhost:8080/auth/publicKey";
+
+      return this.http.get(publicKeyUrl)
+      .map(
+        (response: Response) => {
+            if(response.status == 200)
+            {
+              var pubkey = response.text();
+              console.log(pubkey);
+              var user = JSON.parse(localStorage.getItem('currentUser'));        
+
+              return KJUR.jws.JWS.verifyJWT(user.jwt, pubkey, {alg: ['RS256']});
+            }
+            else
+            {
+              console.error("Unable to retrieve public key.  Status is: " + response.status);
+            }
+        }
+      );
+    }
 
     login(username: string, password: string) {
         var authenticateUrl = "http://localhost:8080/auth/login";
@@ -32,6 +55,17 @@ export class AuthenticationService {
                   console.log(user);
                   user.jwt = jwt;
                   localStorage.setItem('currentUser', JSON.stringify(user));
+
+                  this.validatePublicKey()
+                  .subscribe(
+                      data => {
+                        if(data) { console.log("Public key validated sucessfully"); }
+                        else     { console.error("Invalid public key"); }
+                      },
+                      error => {
+                          console.error(error);
+                      }
+                  )          
                   return user;
                 }
                 else
