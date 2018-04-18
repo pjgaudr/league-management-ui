c2af87529a795e487e18dba7385b450c992151c2
pjgaudr <gaudp00@yahoo.com>
2018-04-18 18:01:38 -0400

    Now handling the 401 error properly on login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AuthenticationService } from '../_services/authentication.service';
 import { LeagueService } from '../_services/league.service';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -18,7 +19,8 @@ export class LoginComponent implements OnInit {
       private route: ActivatedRoute,
       private router: Router,
       private authenticationService: AuthenticationService,
-      private leagueService: LeagueService
+      private leagueService: LeagueService,
+      public snackBar: MatSnackBar
       /*private alertService: AlertService*/) { }
 
   ngOnInit() {
@@ -39,8 +41,9 @@ export class LoginComponent implements OnInit {
                   this.router.navigate(['/welcome']);
               },
               error => {
-                  console.error(error);
                   // this.alertService.error(error);
+                  this.snackBar.open("Invalid username of password. Please try again...", 'Ok', {duration: 3000});            
+                  this.model.password = "";
                   this.loading = false;
               }
           );
